a3b065155e4c592cfdad51302409f6b9cb4c4713
Nixon Augustine <nixon_augustine@hotmail.com>
2020-05-09 23:39:38 +0530

    ngIf ngFor and ngSwitch Tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,15 @@ export class AppComponent {
   title = 'ngIf-ngFor-ngSwitch';
 
   booleanValue : boolean = true;
-  intValue : number = 1;
+  intValue : number = 3;
+  numberArray = [1,2,3,4,5,6,7,8,9];
+  
+  changeValue(){
+    this.booleanValue = !this.booleanValue;
+  }
+
+  changeIntValue(){
+    this.intValue = -this.intValue;
+  }
   
 }
